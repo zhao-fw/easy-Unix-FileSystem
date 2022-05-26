65a7a7afb0003fe0f824ec543f5278e04812e83f
zhao-fw <74136972+zhao-fw@users.noreply.github.com>
2022-05-26 17:06:23 +0800

    增加了磁盘模拟文件的类

--- src/Device.cpp
+++ src/Device.cpp
@@ -1,22 +1,44 @@
-#ifndef DEVICE_H
-#define DEVICE_H
-
-class Device
-{
-public:
-	Device();
-	~Device();
-
-private:
-
-};
+#include "Device.h"
 
 Device::Device()
 {
+	/* 如果文件不存在，创建一个新的文件 */
+	fp = fopen(deviceFile, "a+");
+	fclose(fp);
+
+	/* 以读写方式打开文件 */
+	fp = fopen(deviceFile, "r+");
 }
 
 Device::~Device()
 {
+	if (fp) {
+		fclose(fp);
+	}
 }
 
-#endif
+bool Device::Exist() {
+	return fp != NULL;
+}
+
+void Device::OpenD() {
+	fp = fopen(deviceFile, "wb+");
+	if (fp == NULL) {
+		printf("打开或新建文件%s失败！", deviceFile);
+		exit(-1);
+	}
+}
+
+void Device::WriteD(const void* buffer, unsigned int size, int offset) {
+	if (offset >= 0) {
+		fseek(fp, offset, SEEK_SET);
+	}
+	fwrite(buffer, size, 1, fp);
+}
+
+void Device::ReadD(void* buffer, unsigned int size, int offset) {
+	if (offset >= 0) {
+		fseek(fp, offset, SEEK_SET);
+	}
+	fread(buffer, size, 1, fp);
+}
--- src/Device.h
+++ src/Device.h
@@ -1,22 +1,38 @@
 #ifndef DEVICE_H
 #define DEVICE_H
 
-class Device
-{
-public:
-	Device();
-	~Device();
+#include"File.h"
+#include <cstdio>
+#include <cstdlib>
 
+class Device {
 private:
 
+    /* 磁盘文件句柄，linux下使用 
+     * int fd;
+    */
+
+    /* 磁盘文件指针 linux和Windows都可以使用 */
+    FILE* fp;
+
+    /* 磁盘文件 */
+    const char* deviceFile = "device.img";
+
+public:
+    Device();
+    ~Device();
+
+    /* 检查镜像文件是否存在 */
+    bool Exist();
+
+    /* 打开镜像文件 */
+    void OpenD();
+
+    /* 实际写磁盘函数 */
+    void WriteD(const void* buffer, unsigned int size, int offset = -1);
+
+    /* 实际写磁盘函数 */
+    void ReadD(void* buffer, unsigned int size, int offset = -1);
 };
-
-Device::Device()
-{
-}
-
-Device::~Device()
-{
-}
 
 #endif
